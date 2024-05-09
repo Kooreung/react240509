c875c4846a0836ff092023f275298dc0261d361e
kooreung <crisishyun@gmail.com>
2024-05-09 11:10:22 +0900

    컴포넌트 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+function MyTag() {
+  const hello = <h3>hello component</h3>;
+  return hello;
+}
+
+function MyContent() {
+  return <p>my content</p>;
+}
 
 function App() {
-
   return (
     <>
       <div>
-          <h1>Hello React</h1>
+        <h1>Hello React</h1>
+        <MyTag />
+        {/* <h3> hello component </h3> */}
+        <MyContent />
+        {/* <p> my content </p> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
