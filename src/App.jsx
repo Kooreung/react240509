a583f81b116392270d6a40a10542c2d8ecc76ded
kooreung <crisishyun@gmail.com>
2024-05-10 15:36:56 +0900

    props 활용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+function MyButton(props) {
+  return (
+    <div>
+      <button onClick={props.onClick}>{props.name}</button>
+    </div>
+  );
+}
+
+// function MyButton({onClick, name}) {
+//   return (
+//     <div>
+//       <button onClick={onClick}>{name}</button>
+//     </div>
+//   );
+// }
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton name={"샾 출력"} onClick={() => console.log("####")} />
+      <MyButton name={"느낌표 출력"} onClick={() => console.log("!!!!!")} />
+    </div>
+  );
 }
 
 export default App;
