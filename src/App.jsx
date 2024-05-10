29b51c3b7ccf781d713883c5b4a7c2274fd434ea
kooreung <crisishyun@gmail.com>
2024-05-10 14:13:49 +0900

    props (properties)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
+function MyButton(props) {
+  // props : properties
+  // props : { name : "클릭3" }
+  return (
+    <button
+      style={{
+        padding: "10px",
+        backgroundColor: "lightskyblue",
+        color: "black",
+      }}
+    >
+      {props.name}
+    </button>
+  );
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "deepskyblue",
+          color: "white",
+        }}
+      >
+        클릭1
+      </button>
+      <br />
+      <button
+        style={{
+          padding: "10px",
+          backgroundColor: "deepskyblue",
+          color: "white",
+        }}
+      >
+        클릭2
+      </button>
+      <br />
+      <MyButton name="클릭3" />
+      <MyButton name="클릭4" />
+    </div>
+  );
 }
 
 export default App;
