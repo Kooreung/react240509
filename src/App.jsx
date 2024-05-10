37111e1af3503871b19294025aa5e640f548b316
kooreung <crisishyun@gmail.com>
2024-05-10 15:10:42 +0900

    props 의 값 타입

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 
-function MyButton({ name = "클릭" }) {
-  return <button>{name}</button>;
+function MyComp(props) {
+  return <div></div>;
 }
 
 function App(props) {
   return (
     <div>
-      <MyButton name="버튼1" />
+      {/* props 값은 Javascript 값의 타입을 쓸 수 있다. */}
+      {/* 항상 {} 중괄호가 필요하다. */}
+      {/* String 타입만 중괄호 생략이 가능하다. */}
+      <MyComp
+        name={"son"}
+        age={3.14}
+        address={{ city: "seoul", country: "us" }}
+        foods={["pizza", "coffee"]}
+        married={true}
+        action={function () {
+          console.log("action");
+        }}
+        someAction={() => console.log("someAction")}
+      />
     </div>
   );
 }
