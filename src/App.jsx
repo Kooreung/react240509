425640dfda080dd498f863966ce2ebd46e5689fc
kooreung <crisishyun@gmail.com>
2024-05-10 12:32:40 +0900

    배열을 분해 할당 나머지 값 복사

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,24 @@
 import React from "react";
 
 function App(props) {
-  const a = {
-    name: "John",
-    age: 44,
-    address: {
-      street: "Lorem Ipsum",
-      city: "seoul",
-    },
-  };
+  const a = [6, 7, 8];
+  const [x, y, z] = a;
+  console.log(x, y, z);
 
-  // 얕은 복사 (shallow copy)
-  const { ...b } = a;
-  b.age = 55;
-  console.log("b.age", b.age); // 55
-  console.log("a.age", a.age); // 44
+  const [s, ...t] = a;
 
-  b.address.city = "busan";
-  console.log("a.address.city", a.address.city); // busan
-  console.log("b.address.city", b.address.city); // busan
+  console.log(s, t);
+  console.log(t[0], t[1]);
 
-  // 깊은 복사 (deep copy)
-  // 얕은 복사를 여러번 해서 해결
-  const { ...c } = a; // 얕은 복사
-  const { ...address1 } = a.address;
-  c.address = address1;
-  c.address.city = "london";
-  console.log("a.address.city", a.address.city); // busan
-  console.log("c.address.city", c.address.city); // london
+  const c = a; // 참조 값을 복사. c 값 바꾸면 a도 변경
+  const [...b] = a; // 각 원소를 새 배열에 복사
 
-  const d = {
-    company: {
-      name: "apple",
-      location: "New York",
-    },
-    name: "iphone",
-    price: 300,
-  };
-
-  const { ...e } = d;
-  const { ...company } = d.company;
-  e.company = company;
-  e.name = "galaxy";
-  e.company.name = "samsung";
-
-  console.log("d.name", d.name); // iphone
-  console.log("d.company.name", d.company.name); // apple
-  console.log("e.name", e.name);
-  console.log("e.company.name", e.company.name);
+  c[0] = 66;
+  console.log(c[0]);
+  console.log(a[0]);
+  b[0] = 60;
+  console.log(b[0]);
+  console.log(a[0]);
 
   return <div></div>;
 }
