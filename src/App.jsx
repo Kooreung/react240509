b6a76b2564f2004794bbe069d03a68a4bb9438fd
kooreung <crisishyun@gmail.com>
2024-05-10 15:03:17 +0900

    비어있는 props 기본값 할당

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,13 @@
 import React from "react";
 
-function MyComp({ name, age, team = "무소속" }) {
-  return (
-    <div>
-      <ul>
-        <li>이름 : {name}</li>
-        <li>나이 : {age}</li>
-        <li>팀 : {team}</li>
-      </ul>
-    </div>
-  );
+function MyButton({ name = "클릭" }) {
+  return <button>{name}</button>;
 }
 
 function App(props) {
   return (
     <div>
-      <MyComp name="흥민" age={33} team="토트넘" />
-      <MyComp name="강인" age={22} team="파리" />
-      <MyComp name="길동" age={44} />
+      <MyButton name="버튼1" />
     </div>
   );
 }
