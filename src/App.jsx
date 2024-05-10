51fea7f72a0ea2c96227e2e0d073eed84cd74292
kooreung <crisishyun@gmail.com>
2024-05-10 15:24:39 +0900

    삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-function MyComp({ name, age, someProp, otherProp }) {
-  console.log(someProp);
-  // 이름만 넘어오는 props 의 값은 true
-  console.log(otherProp);
-
-  return <div></div>;
-}
-
 function App(props) {
-  return (
-    <div>
-      <MyComp name={"son"} age={30} someProp otherProp={true} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
