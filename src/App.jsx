8d92e9fa651183bf3658394cb57c8e98a15eaad6
kooreung <crisishyun@gmail.com>
2024-05-10 16:49:46 +0900

    Chakra UI 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
+import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
 
-function App() {
-
+function App(props) {
   return (
-    <>
+    <ChakraProvider>
       <div>
-          <h1>Hello React</h1>
+        <Button colorScheme={"blue"} style={{ marginRight: "1rem" }}>
+          확인
+        </Button>
+        <Button colorScheme={"red"}>취소</Button>
+        <Button
+          sx={{
+            color: "skyblue",
+            marginLeft: "1rem",
+            fontSize: "2rem",
+            padding: "2rem",
+          }}
+          colorScheme={"gray"}
+        >
+          정정
+        </Button>
       </div>
-    </>
-  )
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
