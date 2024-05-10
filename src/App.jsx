04fd3e3e06fa6f78420ecc1ee6ae13e9f5201772
kooreung <crisishyun@gmail.com>
2024-05-10 16:30:27 +0900

    Style props 응용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
+function MyButton({ name, sx }) {
+  return <button style={sx}>{name}</button>;
+}
+
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton
+        name={"버튼1"}
+        sx={{
+          color: "gray",
+          backgroundColor: "skyblue",
+          padding: "10px",
+          width: "50%",
+          fontSize: "2rem",
+        }}
+      />
+      <br />
+      <MyButton
+        name={"버튼2"}
+        sx={{
+          color: "white",
+          backgroundColor: "darkgreen",
+          padding: "10px",
+          width: "50%",
+          fontSize: "2rem",
+          textAlign: "right",
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
