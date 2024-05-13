aa3acfd3a644239963e8536fad68483b9135069a
kooreung <crisishyun@gmail.com>
2024-05-13 16:29:17 +0900

    하나의 state 가 여러 property 를 갖는 경우

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [car, setCar] = useState({
+    name: "",
+    model: "",
+    company: "",
+    price: "",
+  });
+
+  const handleModelChange = (e) => {
+    return setCar({ ...car, model: e.target.value });
+  };
+
+  function handlePriceChange(e) {
+    const { ...nextCar } = car;
+    nextCar.price = e.target.value;
+    setCar(nextCar);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder={"이름"}
+          onChange={(e) => {
+            setCar({ ...car, name: e.target.value });
+          }}
+        />
+      </div>
+      <div>
+        <input type="text" placeholder={"모델"} onChange={handleModelChange} />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder={"회사"}
+          onChange={(e) => {
+            const { ...nextCar } = car;
+            nextCar.company = e.target.value;
+            setCar(nextCar);
+          }}
+        />
+      </div>
+      <div>
+        <input type="text" placeholder={"가격"} onChange={handlePriceChange} />
+      </div>
+      <ul>
+        <li>이름 : {car.name}</li>
+        <li>모델 : {car.model}</li>
+        <li>회사 : {car.company}</li>
+        <li>가격 : {car.price}</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
