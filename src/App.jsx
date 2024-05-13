5945da7fdced9915d5dcf97a605afde599a513e3
kooreung <crisishyun@gmail.com>
2024-05-13 12:38:41 +0900

    Re render 시점에 적절한 순간 선택

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,21 @@
 import React, { useState } from "react";
 
 function MyComp1() {
-  const [val, setVal] = useState([]);
+  const [val, setVal] = useState(0);
 
-  function addItem() {
-    val.push("a");
-    setVal(val);
-    // 출력 안됨. 배열도 객체이기 때문에
-    // 같은 참조값을 가지고 있어서 안됨
-    // 다른 배열이 되어야 한다.
+  function updateVal() {
+    // react는 re render 시점을 적절한 순간으로 선택함
+    setVal(val + 1);
+    setVal(val + 1);
+    setVal(val + 1);
+    console.log("val", val);
   }
 
   return (
     <div>
       <div>{val}</div>
       <div>
-        <button onClick={addItem}>추가</button>
-      </div>
-    </div>
-  );
-}
-
-function MyComp2() {
-  const [val, setVal] = useState([]);
-  function addItem() {
-    // 상태가 객체면 새 객체로 복사하여 사용
-    const [...newVal] = val;
-    newVal.push("a");
-    setVal(newVal);
-  }
-  return (
-    <div>
-      <div>{val}</div>
-      <div>
-        <button onClick={addItem}>추가</button>
+        <button onClick={updateVal}>증가</button>
       </div>
     </div>
   );
@@ -43,7 +25,6 @@ function App(props) {
   return (
     <div>
       <MyComp1 />
-      <MyComp2 />
     </div>
   );
 }
