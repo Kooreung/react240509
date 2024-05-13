df25f7181344455266ccb921b885fe279d0f2a89
kooreung <crisishyun@gmail.com>
2024-05-13 09:29:40 +0900

    Children Prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import * as PropTypes from "prop-types";
+
+function MyButton(props) {
+  return (
+    <div>
+      <button>{props.children}</button>
+    </div>
+  );
+}
+
+MyButton.propTypes = { children: PropTypes.node };
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyButton>클릭1</MyButton>
+      <MyButton>클릭2</MyButton>
+    </div>
+  );
 }
 
 export default App;
