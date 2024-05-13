100cc1ba6f8b63616fb20877f101d834fecf158f
kooreung <crisishyun@gmail.com>
2024-05-13 16:15:40 +0900

    상태예제6) 하나의 state 가 여러 property 를 갖는 경우

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,62 @@
 import React, { useState } from "react";
 
 function App(props) {
-  // 필요 시 여러 state 사용 가능
-  const [age, setAge] = useState("20");
-  const [name, setName] = useState("이름");
-  const [country, setCountry] = useState("국가");
-  const [team, setTeam] = useState("팀");
+  const [person, setPerson] = useState({
+    name: "",
+    address: "",
+    city: "",
+    email: "",
+  });
+
   return (
     <div>
       <div>
         <input
           type="text"
-          onChange={(e) => setAge(e.target.value)}
-          placeholder={"나이"}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          onChange={(e) => setName(e.target.value)}
           placeholder={"이름"}
+          onChange={(e) => {
+            const { ...nextPerson } = person;
+            nextPerson.name = e.target.value;
+            setPerson(nextPerson);
+          }}
         />
       </div>
       <div>
         <input
           type="text"
-          onChange={(e) => setCountry(e.target.value)}
-          placeholder={"국가"}
+          placeholder={"이메일"}
+          onChange={(e) => {
+            const { ...nextPerson } = person;
+            nextPerson.email = e.target.value;
+            setPerson(nextPerson);
+          }}
         />
       </div>
       <div>
         <input
           type="text"
-          onChange={(e) => setTeam(e.target.value)}
-          placeholder={"팀"}
+          placeholder={"도시"}
+          onChange={(e) => {
+            const nextPerson = { ...person, city: e.target.value };
+            setPerson(nextPerson);
+          }}
         />
       </div>
       <div>
-        {name} 의 나이는 {age} 세, {country} 의 {team} 소속입니다.
+        <input
+          type="text"
+          placeholder={"주소"}
+          onChange={(e) => {
+            setPerson({ ...person, address: e.target.value });
+          }}
+        />
       </div>
+      <ul>
+        <li>name: {person.name}</li>
+        <li>email: {person.email}</li>
+        <li>city: {person.city}</li>
+        <li>address: {person.address}</li>
+      </ul>
     </div>
   );
 }
