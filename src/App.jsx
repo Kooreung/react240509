7fee1e45d177094bbdd4453385f1e0d619ae345a
kooreung <crisishyun@gmail.com>
2024-05-13 11:32:28 +0900

    react Component State

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyComp() {
+  let val = 0;
+
+  function increment() {
+    val++;
+  }
+
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
+
+function MyComp2() {
+  // react component 는 상태가 바뀌면 다시 그려짐 (re rendering)
+  // 각 component 의 state 는 react 가 관리
+
+  /* useState = 배열을 return
+  첫 번째 원소(0) : 상태값
+  두 번째 원소(1) : 상태를 업데이트 하는 함수
+  const [ 0 , 1 ] = useState(); */
+
+  // useState 의 파라미터로 초기값 전달
+  const [val, setVal] = useState(0);
+  function increment() {
+    setVal(val + 1);
+  }
+  return (
+    <div>
+      {val}
+      <button onClick={increment}>증가</button>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyComp />
+      <MyComp2 />
+    </div>
+  );
 }
 
 export default App;
