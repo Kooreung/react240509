a456a5ebd9ccb99b8d4c5c7077b97f59065ff9ce
kooreung <crisishyun@gmail.com>
2024-05-13 10:32:16 +0900

    react Component 를 만드는 방법들

---
+++ src/MyComps.jsx
@@ -0,0 +1,11 @@
+// react Component 들
+
+export function MyComps() {
+  return <div>myComp</div>;
+}
+
+export const YourComp = function () {
+  return <div>yourComp</div>;
+};
+
+export const ArrowComp = () => <div>arrow function component</div>;
