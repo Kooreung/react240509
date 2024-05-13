7d938c6315bbd9408206f6e4d4b9af9f759947e0
kooreung <crisishyun@gmail.com>
2024-05-13 14:11:35 +0900

    State 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyNumber() {
+  const [number, setNumber] = useState(1000);
+
+  return (
+    <div>
+      <div>{number}</div>
+      <div>
+        <button onClick={() => setNumber(number - 1)}>1씩 감소</button>
+        <button onClick={() => setNumber(number - 10)}>10씩 감소</button>
+        <button onClick={() => setNumber(number / 10)}>10씩 나누기</button>
+      </div>
+      <div>
+        <button onClick={() => setNumber(number + 1)}>1씩 증가</button>
+        <button onClick={() => setNumber(number + 10)}>10씩 증가</button>
+        <button onClick={() => setNumber(number * 10)}>10씩 곱하기</button>
+      </div>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyNumber />
+    </div>
+  );
 }
 
 export default App;
