5568ac2e8aae5a9ba669b3efb7aa3c6da3479d5f
kooreung <crisishyun@gmail.com>
2024-05-13 11:14:59 +0900

    코드 리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 import React from "react";
-import val, { v1 } from "./MyValues.jsx";
-import someValue from "./MyComps.jsx";
 
 function App(props) {
-  return (
-    <div>
-      <div>{v1}</div>
-      <div>{val}</div>
-      <div>{someValue}</div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
