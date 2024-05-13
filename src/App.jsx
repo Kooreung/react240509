f6e761d8e4cb649e6af41adb3af959f270c18cd4
kooreung <crisishyun@gmail.com>
2024-05-13 15:46:05 +0900

    여러 State 사용 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [name, setName] = useState("이름");
+  const [hobby, setHobby] = useState("취미");
+  const [address, setAddress] = useState("주소");
+  const [city, setCity] = useState("도시");
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          placeholder={"이름"}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder={"취미"}
+          onChange={(e) => setHobby(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder={"주소"}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder={"도시"}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </div>
+      <div>
+        {name}, {hobby}, {address}, {city}
+      </div>
+    </div>
+  );
 }
 
 export default App;
