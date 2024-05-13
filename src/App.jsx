5cf94866feee6c9bbbe1fe4e0c0521666a2ff065
kooreung <crisishyun@gmail.com>
2024-05-13 15:05:03 +0900

    state 연습

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import React, { useState } from "react";
 function App(props) {
   const [text, setText] = useState("");
 
-  function handleUpdateText(e) {
-    // console.log(event.target.value);
-    setText(e.target.value);
-  }
+  const isTooLong = text.length > 10;
   return (
     <div>
-      <input type="text" onChange={handleUpdateText} />
+      <input type="text" onChange={(e) => setText(e.target.value)} />
       <p>{text}</p>
+      {isTooLong && <p>10 자 이상 입력할 수 없습니다.</p>}
     </div>
   );
 }
