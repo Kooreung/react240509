c1eb72deb19152c3db38ab8bf8e4d29255a62f41
kooreung <crisishyun@gmail.com>
2024-05-13 15:32:45 +0900

    input 에 입력한 값 list 에 추가하고 보여주기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, ChakraProvider, Input } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+
+  function handleUpload() {
+    if (text.length > 0) {
+      // 공백은 추가 안하게 함
+      let [...nextList] = list;
+      nextList.push(text);
+      setList(nextList);
+      setText(""); // 입력 칸 초기화
+    }
+  }
+
+  return (
+    <ChakraProvider>
+      <div>
+        <Input
+          value={text} // 입력 칸 초기화
+          type="text"
+          onChange={(e) => setText(e.target.value)}
+        />
+        <Button onClick={handleUpload}>등록</Button>
+      </div>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </ChakraProvider>
+  );
 }
 
 export default App;
