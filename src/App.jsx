83ab46ae6a201f48136828afd00b3f29c30e3397
kooreung <crisishyun@gmail.com>
2024-05-13 15:41:32 +0900

    상태예제5) 여러 State 사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  // 필요 시 여러 state 사용 가능
+  const [age, setAge] = useState("20");
+  const [name, setName] = useState("이름");
+  const [country, setCountry] = useState("국가");
+  const [team, setTeam] = useState("팀");
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setAge(e.target.value)}
+          placeholder={"나이"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder={"이름"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setCountry(e.target.value)}
+          placeholder={"국가"}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          onChange={(e) => setTeam(e.target.value)}
+          placeholder={"팀"}
+        />
+      </div>
+      <div>
+        {name} 의 나이는 {age} 세, {country} 의 {team} 소속입니다.
+      </div>
+    </div>
+  );
 }
 
 export default App;
