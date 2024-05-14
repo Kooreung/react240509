1470ef448a22e18f50de7e1527650c64f4327c1f
kooreung <crisishyun@gmail.com>
2024-05-14 15:43:40 +0900

    ajax Get Request to Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import axios from "axios";
 
 function App(props) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("mount 될 때");
-  }, []);
+  function handleClick() {
+    console.log("버튼 클릭");
+    // get request
+    // 첫 Parameter : 요청 경로
+    axios.get("/someURL");
+  }
 
-  useEffect(() => {
-    console.log("count 가 변경 될 때");
-    // 해당 param 에서 dependency 가 변경되지 않아야 한다.
-  }, [count]);
+  function handleClick2() {
+    const qs = "name=son&age=33";
+    axios.get("/someURL2?" + qs);
+  }
+
+  function handleClick3() {
+    const qs = new URLSearchParams();
+    qs.append("name", "흥민");
+    qs.append("age", "33");
+    qs.append("country", "한국");
+    axios.get("/someURL3?" + qs.toString());
+  }
 
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Click</button>
+      <div>
+        <button onClick={handleClick}>Get 요청</button>
+      </div>
+      <div>
+        <button onClick={handleClick2}>Get 요청 with QueryString</button>
+      </div>
+      <div>
+        <button onClick={handleClick3}>Get 요청 with QueryString</button>
+      </div>
     </div>
   );
 }
