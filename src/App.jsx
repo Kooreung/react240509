0cdf4b1605c1ba8607a1a99b60d2e4f0a8d4aedd
kooreung <crisishyun@gmail.com>
2024-05-14 14:29:54 +0900

    useEffect : 특정 순간에 실행되는 메소드

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const TextContext = createContext(null);
+function MyComp() {
+  // 특정한 순간에 실행되는 메소드를 Parameter 로 받는다.
+  // 예) initial render 가 진행 될 때(Mount),
+  // re-render 가 될 때,
+  // Component 가 Tree 에서 떨어질 때(Unmount),
+  // State 가 바뀔 때 등
 
-function MyInput() {
-  const textHandler = useContext(TextContext);
+  // 1번 Parameter : 실행되는 method
+  // 2번 Parameter : Dependency (특정 순간)
+
+  // 2번 파라미터가 없을 때는 rendering 될 때마다 실행
+  // CLICK 누르면 실행
+  useEffect(() => {
+    console.log("Component loaded");
+  });
+
+  // 2번 파라미터에 빈 배열을 넣으면 initial rendering 때만 실행
+  useEffect(() => {
+    console.log("첫 렌더링 실행때만 실행");
+  }, []);
+
+  const [count, setCount] = useState(0);
 
   return (
     <div>
-      <input
-        type="text"
-        onChange={(e) => textHandler.updateText(e.target.value)}
-      />
-      <p>{textHandler.text}</p>
-    </div>
-  );
-}
-
-function MyText() {
-  const textHandler = useContext(TextContext);
-
-  return (
-    <div>
-      <p>{textHandler.text}</p>
+      hello comp
+      <button onClick={() => setCount(count + 1)}>CLICK</button>
     </div>
   );
 }
 
 function App(props) {
-  const [text, setText] = useState("");
-
-  function handleUpdateText(t) {
-    setText(t);
-  }
-
+  const [show, setShow] = useState(true);
   return (
     <div>
-      <TextContext.Provider value={{ text, updateText: handleUpdateText }}>
-        <MyInput />
-        <MyText />
-      </TextContext.Provider>
+      <input
+        type="checkbox"
+        checked={show}
+        onChange={(e) => setShow(e.target.checked)}
+      />
+      {show && <MyComp />}
     </div>
   );
 }
