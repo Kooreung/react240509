7c3faafab3313eab06286a93ca42434b0e3a3b91
kooreung <crisishyun@gmail.com>
2024-05-14 10:24:14 +0900

    lift state up 으로 Component State 유지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox({ count, setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>버튼</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [display, setDisplay] = useState(true);
+  // state 를 상위로 옮겨야 한다.
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={display}
+        onChange={(e) => {
+          setDisplay(e.target.checked);
+        }}
+      />
+      {display && <MyBox count={count} setCount={setCount} />}
+    </div>
+  );
 }
 
 export default App;
