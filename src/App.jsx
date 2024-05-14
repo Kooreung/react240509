05cfd1cf095b399e1b71d292e0bd277a1554a4fe
kooreung <crisishyun@gmail.com>
2024-05-14 09:29:55 +0900

    각자 개별적 state 를 갖는 Component 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [text, setText] = useState("내용을 입력하세요");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <MyBox />
+      <hr />
+      <MyBox />
+    </div>
+  );
 }
 
 export default App;
