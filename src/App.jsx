775259ee6fb1826999df01f73334ef5d17476907
kooreung <crisishyun@gmail.com>
2024-05-14 10:18:34 +0900

    Component 가 Tree 에서 떨어지고 다시 붙으면 Initial Rendering 연습

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
+function MyBox() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ width: "100%", border: "1px solid gray" }}>
+      <button onClick={() => setCount(count + 1)}>버튼</button>
+      <p>{count}</p>
+    </div>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [view, setView] = useState(true);
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={view}
+        onChange={(e) => setView(e.target.checked)}
+      />
+      {view && <MyBox />}
+    </div>
+  );
 }
 
 export default App;
