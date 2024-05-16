c13bf25766c5ccaf9c3ec5150fe81bb50fc5da76
kooreung <crisishyun@gmail.com>
2024-05-16 15:39:27 +0900

    응답 코드 확인

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  return (
+    <div>
+      <button onClick={() => axios.get("/api/main43/sub1")}>200 응답</button>
+      <button onClick={() => axios.get("/api/main43/sub2")}>400 응답</button>
+      <button onClick={() => axios.get("/api/main43/sub401")}>401 응답</button>
+      <button onClick={() => axios.get("/api/main43/sub403")}>403 응답</button>
+      <button onClick={() => axios.get("/api/main43/sub404")}>404 응답</button>
+      <button onClick={() => axios.get("/api/main43/sub500")}>500 응답</button>
+    </div>
+  );
 }
 
 export default App;
