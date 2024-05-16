ba2c2b9b41cdea9654f9067dcf43d10e27af86e1
kooreung <crisishyun@gmail.com>
2024-05-16 10:05:04 +0900

    get, post ajax 요청 버튼 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  function handleClickButton1() {
+    // ajax 형태의 get 요청을 할 것임
+    // 우리는 경로에 api 붙일 건데 spring 포트랑 매칭 시키는 역할임
+    // vite.config 에서 설정해놨음
+    axios.get("/api/someurl");
+  }
+
+  function handleClickButton2() {
+    axios.post("/api/someurl");
+  }
+
+  function handleClickButton11() {
+    axios.get("/api/someurl2");
+  }
+
+  function handleClickButton22() {
+    axios.post("/api/someurl2");
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={handleClickButton1}>get 요청</button>
+        <button onClick={handleClickButton2}>post 요청</button>
+      </div>
+      <div>
+        <button onClick={handleClickButton11}>get 요청2</button>
+        <button onClick={handleClickButton22}>post 요청2</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
