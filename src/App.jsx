1802322850e8cf8b3e316b2604be975e5060472a
kooreung <crisishyun@gmail.com>
2024-05-16 16:33:28 +0900

    Finally 응답 코드와 관계 없이 싱행되는 Method

--- src/App.jsx
+++ src/App.jsx
@@ -38,11 +38,45 @@ function App(props) {
       });
   }
 
+  function handleCode() {
+    const r = Math.random();
+    let path = "";
+    if (r < 1 / 3) {
+      path = "/api/main43/sub1";
+    } else if (r < 2 / 3) {
+      path = "/api/main43/sub401";
+    } else {
+      path = "/api/main43/sub403";
+    }
+    axios
+      .get("path")
+      .then((res) => {
+        console.log("200번 실행");
+      })
+      .catch((err) => {
+        const code = err.response.status;
+
+        switch (code) {
+          case 401:
+            console.log("401 응답 입니다. 로그인되지 않았습니다.");
+            break;
+          case 403:
+            console.log("403 응답 입니다. 권한이 없습니다.");
+            break;
+        }
+      })
+      .finally(() => {
+        console.log("항상 실행");
+      });
+  }
+
   return (
     <div>
       <button onClick={handle200}>200 응답</button>
       <button onClick={handle400}>400 응답</button>
       <button onClick={handle500}>500 응답</button>
+      {/* 200, 401, 403 응답일 때 코드 작성 */}
+      <button onClick={handleCode}>응답 코드에 따라 다른 일</button>
     </div>
   );
 }
