78cbd770e09a1cd4b44b51af5eb72b23a186d3b4
kooreung <crisishyun@gmail.com>
2024-05-16 15:54:08 +0900

    200 외 응답은 then 이 아닌 catch method

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,37 @@ import React from "react";
 import axios from "axios";
 
 function App(props) {
+  function handleClick200() {
+    axios.get("/api/main43/sub1").then((response) => console.log("200응답"));
+  }
+
+  function handleClick400() {
+    axios
+      .get("/api/main43/sub2")
+      .then((response) => console.log("400응답"))
+      .catch(() => console.log("catch 의 Method, 400 응답 시"));
+  }
+
+  function handleClick404() {
+    axios
+      .get("/api/main43/sub404")
+      .then((response) => console.log("404응답"))
+      .catch(() => console.log("catch 의 Method, 404 응답 시"));
+  }
+
+  function handleClick500() {
+    axios
+      .get("/api/main43/sub500")
+      .then((response) => console.log("500번 응답 시"))
+      .catch(() => console.log("catch Method , 500 응답 시"));
+  }
+
   return (
     <div>
-      <button onClick={() => axios.get("/api/main43/sub1")}>200 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub2")}>400 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub401")}>401 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub403")}>403 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub404")}>404 응답</button>
-      <button onClick={() => axios.get("/api/main43/sub500")}>500 응답</button>
+      <button onClick={handleClick200}>200 응답</button>
+      <button onClick={handleClick400}>400 응답</button>
+      <button onClick={handleClick404}>404 응답</button>
+      <button onClick={handleClick500}>500 응답</button>
     </div>
   );
 }
