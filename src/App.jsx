7de1e6a5bd67571760e9af188470c850499ce0d2
kooreung <crisishyun@gmail.com>
2024-05-17 15:42:19 +0900

    index path

--- src/App.jsx
+++ src/App.jsx
@@ -30,6 +30,9 @@ function SpringRoot() {
         {/* Link to : react-router 쓸 때 쓰는 링크 연결 */}
         {/* 페이지 전체 새로고침이 아닌 Component 연결 */}
         <div>
+          <Link to="/spring">home</Link>
+        </div>
+        <div>
           <Link to="/spring/learn">learn</Link>
         </div>
         <div>
@@ -56,6 +59,8 @@ function App(props) {
       // 하위 path 는 상위 path element 에 포함되어 있다.
       // 보이게 하기 위해 Outlet 사용
       children: [
+        // index : 상위 path 만 입력 시 나오는 component 로 설정
+        { index: true, element: <div>spring main page</div> },
         { path: "api", element: <div>api page</div> },
         { path: "doc", element: <div>doc page</div> },
         { path: "learn", element: <div>learn page</div> },
