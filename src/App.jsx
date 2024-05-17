0655e60f1b7d82b905930eb74029807d736bbcec
kooreung <crisishyun@gmail.com>
2024-05-17 14:26:21 +0900

    React-router 로 경로 별 Component 표현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// /about
+// /search
+// /home
+// 위 경로에 다른 컴포넌트 출력 되도록
+const router = createBrowserRouter([
+  { path: "/about", element: <div>about page</div> },
+  { path: "/search", element: <div>search page</div> },
+  { path: "/home", element: <div>home page</div> },
+]);
 
 function App(props) {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
