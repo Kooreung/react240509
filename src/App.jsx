0e75620d0ceb8bff34387d4cdf0315d6190f0071
kooreung <crisishyun@gmail.com>
2024-05-17 14:30:34 +0900

    경로 별 Component 표현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BoardAdd } from "./BoardAdd.jsx";
+import { BoardEdit } from "./BoardEdit.jsx";
+import { BoardList } from "./BoardList.jsx";
 
 function App(props) {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: "/add", element: <BoardAdd /> },
+    {
+      path: "/edit",
+      element: <BoardEdit />,
+    },
+    { path: "/list", element: <BoardList /> },
+  ]);
+  // 각 객체는 path 와 element 보유
+  return <RouterProvider router={router} />;
 }
 
 export default App;
