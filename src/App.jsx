f663b31a37c5343f3106852d71a4d8b9399661ad
kooreung <crisishyun@gmail.com>
2024-05-17 15:14:09 +0900

    상위 path Component 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
+function SpringRoot() {
+  return (
+    <div>
+      <div
+        style={{
+          border: "1px solid black",
+          height: "50px",
+          textAlign: "center",
+          marginBottom: "1rem",
+        }}
+      >
+        상단 NAVBAR
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
 function App(props) {
   const router = createBrowserRouter([
     {
       path: "spring",
-      element: (
-        <div>
-          spring root page <Outlet />
-        </div>
-      ),
+      element: <SpringRoot />,
       // 상위 path 에 element 가 있으면
       // 하위 path 는 상위 path element 에 포함되어 있다.
       // 보이게 하기 위해 Outlet 사용
