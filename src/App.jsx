819b45abbb6f40b5c71abfe6193c75aacfa96b1d
kooreung <crisishyun@gmail.com>
2024-05-17 16:23:52 +0900

    Dynamic Segment (경로의 일부분을 데이터로 활용)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {
   Link,
   Outlet,
   RouterProvider,
+  useParams,
 } from "react-router-dom";
 
 function Root() {
@@ -24,11 +25,24 @@ function Root() {
 function BoardList() {
   return (
     <div>
-      <div>1번 게시물 보기</div>
-      <div>2번 게시물 보기</div>
-      <div>3번 게시물 보기</div>
+      <div>
+        {/* 경로의 일부분으로 전환 */}
+        <Link to={"/board/1"}>1번 게시물 보기</Link>
+      </div>
+      <div>
+        <Link to={"/board/2"}>2번 게시물 보기</Link>
+      </div>
+      <div>
+        <Link to={"/board/3"}>3번 게시물 보기</Link>
+      </div>
     </div>
   );
+}
+
+function BoardView() {
+  // Dynamic segment 읽기
+  const params = useParams();
+  return <div>{params.id}번 게시물 보기</div>;
 }
 
 const router = createBrowserRouter([
@@ -41,8 +55,8 @@ const router = createBrowserRouter([
         element: <div>MAIN PAGE</div>,
       },
       {
-        path: "board",
-        element: <div>BOARD PAGE</div>,
+        path: "board/:id",
+        element: <BoardView />,
       },
       {
         path: "list",
