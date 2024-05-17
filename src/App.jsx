40ece784615067e1731047c940ffc57e7b9a03b0
kooreung <crisishyun@gmail.com>
2024-05-17 14:19:54 +0900

    코드리셋

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App(props) {
-  const router = createBrowserRouter([
-    { path: "/add", element: <div>글 작성 페이지</div> },
-    {
-      path: "/edit",
-      element: <div>글 수정 페이지</div>,
-    },
-    { path: "/list", element: <div>글 목록 페이지</div> },
-  ]);
-  // 각 객체는 path 와 element 보유
-  return <RouterProvider router={router} />;
+  return <div></div>;
 }
 
 export default App;
