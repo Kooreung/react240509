3f6c09c3deb53d71665c38a20385c41c443b065e
kooreung <crisishyun@gmail.com>
2024-05-20 10:41:36 +0900

    Chakra UI hover pseudo Class

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ function App(props) {
   return (
     <ChakraProvider>
       <div>
-        <Box bgColor={"tomato"} w={"500px"}>
-          Lorem ipsum dolor.
-        </Box>
-        <Box bgColor={"yellow.700"} w={{ base: "100%", md: "500px" }}>
+        <Box
+          bgColor={"orange"}
+          _hover={{
+            bgColor: "yellow",
+          }}
+        >
           Lorem ipsum dolor.
         </Box>
       </div>
