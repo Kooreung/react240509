3b0caaa0f6079f934ae62b4bc3c7b5bf1c651b09
kooreung <crisishyun@gmail.com>
2024-05-20 12:35:14 +0900

    React hook (use...) 은 Component 상단에

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
-import React from "react";
-import { Button, ChakraProvider } from "@chakra-ui/react";
-import { EmailIcon, StarIcon } from "@chakra-ui/icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, useDisclosure, useToast } from "@chakra-ui/react";
 
 function App(props) {
+  const toast = useToast();
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [count, setCount] = useState();
+  useEffect(() => {}, []);
+  // use... -> 이와 같은 형태의 Method 를 hook 이라고 한다.
+  // hook 은 진행 순서가 남는다.
+  // if / for 등 반복문 내에서 사용하면 안된다.
+  // hook 은 Component 의 최상단에 순서대로 작성하면 안전하게 사용 가능하다.
+
   return (
     <ChakraProvider>
-      <div>
-        <StarIcon />
-      </div>
-      <div>
-        <Button leftIcon={<EmailIcon />}>Email</Button>
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faTwitter} />
-      </div>
+      <div></div>
     </ChakraProvider>
   );
 }
