fef0b66415e9751376f8a4ccd06228405cca2764
kooreung <crisishyun@gmail.com>
2024-05-20 14:19:32 +0900

    stopPropagation / Event Bubbling 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Box, Button, ChakraProvider } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  function handleButtonClick(e) {
+    // event bubbling 멈추는 Method 사용
+    e.stopPropagation();
+    console.log("Button click");
+  }
+
+  function handleBoxClick() {
+    console.log("Box click");
+  }
+
+  return (
+    <ChakraProvider>
+      <Box w={"100px"} h={"100px"} bgColor={"orange"} onClick={handleBoxClick}>
+        <Button onClick={handleButtonClick}>클릭</Button>
+      </Box>
+    </ChakraProvider>
+  );
 }
 
 export default App;
