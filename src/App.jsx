6269d44a63889e31fecfc9554e42cb008d3dfbe5
kooreung <crisishyun@gmail.com>
2024-05-20 15:01:57 +0900

    preventDefault / 기본 이벤트 동작 방지

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function App(props) {
-  return <div></div>;
+  // a 클릭 시
+  // form 서브밋 시
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log("다른일");
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("다른일");
+  }
+
+  // 기본 동작 막고,, 콘솔에 간단한 메세지 출력
+  return (
+    <div>
+      <a href="https://www.google.com" onClick={handleClick}>
+        구글
+      </a>
+      <hr />
+      <form action="https://www.google.com" onSubmit={handleSubmit}>
+        <input type="text" />
+        <button>전송</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
