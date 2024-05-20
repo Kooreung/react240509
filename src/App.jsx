7dded1c6801c6273189b373d8a5213d8a46d5e3f
kooreung <crisishyun@gmail.com>
2024-05-20 11:19:14 +0900

    Chakra UI Modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  Button,
+  ChakraProvider,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  return (
+    <ChakraProvider>
+      <Button onClick={onOpen}>모달 버튼</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay>
+          <ModalContent>
+            <ModalHeader> Title </ModalHeader>
+            <ModalBody>모달 콘텐츠</ModalBody>
+            <ModalFooter>
+              <Button onClick={onClose} colorScheme={"blue"}>
+                확인
+              </Button>
+              <Button onClick={onClose}>닫기</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+    </ChakraProvider>
+  );
 }
 
 export default App;
