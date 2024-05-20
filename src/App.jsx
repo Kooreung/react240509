db68da38cf418bbe1739c2719bd4a8bb895bfb4e
kooreung <crisishyun@gmail.com>
2024-05-20 11:49:22 +0900

    Chakra UI Toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
 import React from "react";
-import {
-  Button,
-  ChakraProvider,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
+import { Button, ChakraProvider, useToast } from "@chakra-ui/react";
 
 function App(props) {
-  const { isOpen, onClose, onOpen } = useDisclosure();
+  const toast = useToast();
 
   return (
     <ChakraProvider>
-      <Button onClick={onOpen}>모달 버튼</Button>
-      <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay>
-          <ModalContent>
-            <ModalHeader> Title </ModalHeader>
-            <ModalBody>모달 콘텐츠</ModalBody>
-            <ModalFooter>
-              <Button onClick={onClose} colorScheme={"blue"}>
-                확인
-              </Button>
-              <Button onClick={onClose}>닫기</Button>
-            </ModalFooter>
-          </ModalContent>
-        </ModalOverlay>
-      </Modal>
+      <Button
+        onClick={() => {
+          toast({
+            title: "토스트 제목",
+            description: "토스트 내용",
+            position: "top",
+            status: "info",
+            isClosable: true,
+            duration: 1000,
+          });
+        }}
+      >
+        aaa
+      </Button>
     </ChakraProvider>
   );
 }
